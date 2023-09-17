c755f2962f7761e2d8b78c88842f0c853d9b0902
aomwara <waranat.cpe@gmail.com>
2023-09-17 13:20:05 +0700

    feat: initial nextjs and libs

---
+++ nextjs/src/pages/_app.tsx
@@ -0,0 +1,44 @@
+import type { AppProps } from "next/app";
+import "@rainbow-me/rainbowkit/styles.css";
+
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum, base, zora } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+import { ChakraProvider } from "@chakra-ui/react";
+
+const { chains, publicClient } = configureChains(
+  [mainnet, polygon, optimism, arbitrum, base, zora],
+  [
+    alchemyProvider({ apiKey: process.env.ALCHEMY_ID as string }),
+    publicProvider(),
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  projectId: "YOUR_PROJECT_ID",
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <ChakraProvider>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+};
+
+export default App;
---
+++ nextjs/src/views/pages/HomePage.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export const HomePage = () => {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+        <meta name="description" content="Home page" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main>
+        <h1>Home Page</h1>
+      </main>
+    </>
+  );
+};
